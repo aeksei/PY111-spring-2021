47a539c52568bc4d0dcb0d3df445af4f88614635
alekseyp <aeksei94@gmail.com>
2021-05-20 20:21:23 +0300

    исправлена функция peek

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -34,9 +34,11 @@ def peek(ind: int = 0) -> Any:
     Allow you to see at the element in the stack without popping it.
 
     :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
-    :return: peeked element or None if no element in this place
+    :return: peeked element or None
+    if no element in this place
     """
-    return my_stack[-1 - ind]
+    if 0 <= ind < len(my_stack):
+        return my_stack[-1 - ind]
 
 
 def clear() -> None:
